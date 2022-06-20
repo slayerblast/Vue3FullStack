dd8027b92583f6c24d4bbd79d1bd34f019daf2b5
slayerblast <hery_75@hotmail.fr>
2022-06-20 16:07:10 +0200

    ajout du formulaire de connexion

--- src/components/Connexion.vue
+++ src/components/Connexion.vue
@@ -3,5 +3,18 @@ import {ref} from "vue"
 </script>
 
 <template>
+    <div class="container">
+        <form>
+            <div class="mb-3">
+              <label for="exampleInputEmail1" class="form-label">adresse email</label>
+              <input type="email" class="form-control" id="exampleInputEmail1">
+            </div>
+            <div class="mb-3">
+              <label for="exampleInputPassword1" class="form-label">mot de passe</label>
+              <input type="password" class="form-control" id="exampleInputPassword1">
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+          </form>
+    </div>
 
 </template>
